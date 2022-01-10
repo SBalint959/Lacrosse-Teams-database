61718d63cbf0a9b45827dd7edf2d585ad906ffdd
SBalint959 <75448464+SBalint959@users.noreply.github.com>
2022-01-10 21:04:45 +0100

    Update index.routes.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require('express-openid-connect');
+const db = require('../db');
+const app = express();
+const fs = require('fs');
+const { Parser } = require('json2csv');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: '9uXfHuiv9lYR42LkAvUzxe8H4un2tyOYYWin3ymJxr-TAsCT1J20hxg73cvw6sIC',
+  baseURL: 'http://localhost:3000',
+  clientID: 'aNSzhblfAMBowH1WDz8zlXxoeXBogKRJ',
+  issuerBaseURL: 'https://dev-xxdzkni1.us.auth0.com'
+};
+
+router.use(auth(config));
+
+router.get('/profile', (req, res) => {
+   if(req.oidc.user === undefined){
+       res.send("Acces denied. You are not logged in!")
+   }
+   else{
+   res.render('profile', {
+     title: "Profile - " + req.oidc.user.nickname,
+     data: req.oidc.user
+   });
+   }
+});
+
+router.get('/update', async function (req, res, next) {
+    if(req.oidc.user === undefined){
+        res.send("Acces denied. You are not logged in!")
+    } else {
+        const sqlPodaci =  `SELECT * FROM topplayerteams NATURAL JOIN topplayers`;
+        try {
+            const resultPodaci = (await db.query(sqlPodaci, [])).rows;
+
+            fs.writeFile('./NLLTeams.json', JSON.stringify(resultPodaci, null, 3), (err) => {
+                if(err){
+                    console.log(err);
+                }
+            });
+            const parser = new Parser({
+                escapedQuote: '"'
+            });
+            try{
+                let csvFile = parser.parse(resultPodaci);
+                fs.writeFile('./NLLTeams.csv', csvFile,(err) => {
+                    if(err){
+                        console.log(err);
+                    }
+                });
+           }
+            catch(err){
+                console.log(err);
+            }
+            res.render('updatedFiles');
+
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+});
 
 router.get('/', function (req, res, next) {
     res.render('index', {
         title: 'Main page',
-        linkActive: 'index'
+        data: req.oidc.user
     });
 });
 
